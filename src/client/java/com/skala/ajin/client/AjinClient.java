43c3703194c053d8103861531e648e1223152d46
Skala <dw1720718@gmail.com>
2024-10-09 14:18:17 +0100

    start project

---
+++ src/client/java/com/skala/ajin/client/AjinClient.java
@@ -0,0 +1,10 @@
+package com.skala.ajin.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AjinClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/skala/ajin/Ajin.java
@@ -0,0 +1,10 @@
+package com.skala.ajin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ajin implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
